762fea748689701cb09e2a06dad5b51613f164c9
rinp <rinp.mail.mail@gmail.com>
2015-09-11 14:17:29 +0900

    euler3の実装完了

--- src/prob3.rs
+++ src/prob3.rs
@@ -1,57 +1,38 @@
-//
-//// #What is the largest prime factor of the number 600851475143 ?
-//
-//// pub fn exe(x:i32)->i32{
-////     let a = 4;
-////     a
-//// }
-//
-////pub fn prime(i:u32)->Vec<u32>{
-////    for x in (2..i){
-////    }
-////    (2..i).filter(|x|i%x==0).collect::<Vec<u32>>()
-////}
-//
-//pub fn divide(i:u32,x:u32)->(u32,u32){
-//    if x%i==0 {
-//        (i,divide(i,x%i).1)
-//    }else{
-//        (i,x)
-//    }
-//}
-//
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//    //use super::test::Bencher;
-//
-////    #[test]
-////    fn test_prime1(){
-////        assert_eq!(prime(10),[2,5]);
-////    }
-////    //13195 are 5, 7, 13 and 29.
-////    #[test]
-////    fn test_prime2(){
-////        assert_eq!(prime(13195),[5, 7, 13, 29]);
-////    }
-//
-//    // #[test]
-//    // fn test_exe() {
-//    //     assert_eq!([], exe(600_851_475_143u32));
-//    // }
-//    //
-//    // #[test]
-//    // fn test_exe() {
-//    //     assert_eq!([5, 7, 13,29], exe(13_195u32));
-//    // }
-//    //
-//    // #[bench]
-//    // fn bench_fib_30(b: &mut Bencher) {
-//    //     b.iter(|| super::fib(30));
-//    // }
-//    //
-//    // #[bench]
-//    // fn bench_exe(b: &mut Bencher) {
-//    //     b.iter(||exe(600_851_475_143u32));
-//    // }
-//}
+extern crate test;
+pub fn exe(x:u64)->u64{
+    search_largest_prime(2,x)
+}
+
+fn search_largest_prime(x:u64,y:u64)->(u64){
+    if x>=y{
+        return x;
+    }else if y%x==0 {
+        return search_largest_prime(x,y/x);
+    }else {
+        return search_largest_prime(x+1,y);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+   use super::test::Bencher;
+
+   #[test]
+   fn test_divide(){
+       assert_eq!(super::search_largest_prime(2,10),5);
+   }
+   #[test]
+   fn test_divide2(){
+       assert_eq!(super::search_largest_prime(2,13195),29);
+   }
+   #[test]
+   fn test_exe() {
+       assert_eq!(6857, exe(600_851_475_143));
+   }
+
+   #[bench]
+   fn bench_exe(b: &mut Bencher) {
+       b.iter(||exe(600_851_475_143));
+   }
+}

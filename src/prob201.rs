9a8271d7df0dd605487fa812b97114036d49eebe
rinp <rinp.mail.mail@gmail.com>
2015-10-16 19:10:32 +0900

    エラー修正

--- src/prob201.rs
+++ src/prob201.rs
@@ -1,28 +1,30 @@
 use std::collections::HashMap;
 
 pub fn exe(array:Vec<u32>,n:u32)->u32{
-    let mut map:HashMap<u64,Vec<u32>> = HashMap::new();
-    let m = calc(map,n);
-    let c:u32 = m.iter().filter(|&(k,v)|v.contains(&1)).count() as u32;
-    let s:u32 = array.iter().sum();
+    let map:HashMap<u64,Vec<u32>> = HashMap::new();
+    let m = calc(map,n as usize);
+    let c:u32 = m.values().filter(|&v|v.contains(&1u32)).count() as u32;
+    let s:u32 = array.iter().fold(0,|s,n|s+n);
     s*c
 }
 
-fn calc(map:HashMap<u64,Vec<u32>>,max:u32)->HashMap<u64,Vec<u32>>{
-    match map.len() {
-        max=> map,
+fn calc(map:HashMap<u64,Vec<u32>>,max:usize)->HashMap<u64,Vec<u32>>{
+
+    match  map.len()  {
+        x if x== max =>map,
         _=>{
             let mut new_map:HashMap<u64,Vec<u32>> = HashMap::new();
 
             map.iter().fold((),|_,(k,v)|{
                 let m = v.iter().max().unwrap();
                 ((m+1u32)..100).fold((),|_,x|{
-                    v.insert(x);
-                    new_map.insert(*k +x as u64,*v);
+                    let mut v2:Vec<u32> =v.clone();
+                    v2.push(x);
+                    new_map.insert(*k +x as u64,v2);
                 });
             });
             calc(new_map,max)
-        }
+        },
     }
 }
 
@@ -39,13 +41,13 @@ mod tests{
 
     #[test]
     fn test_exe2(){
-        let v = (1u32..101).map(|x|x.pow(2)).collect();
+        let v:Vec<u32> = (1u32..101).map(|x|x.pow(2)).collect::<Vec<u32>>();
         assert_eq!(super::exe(v,50),answer::ANSWER_201);
     }
 
-    #[bench]
-    fn bench_exe(b: &mut Bencher) {
-        let v = (1u32..101).map(|x|x.pow(2)).collect();
-        b.iter(||super::exe(v,50));
-    }
+    // #[bench]
+    // fn bench_exe(b: &mut Bencher) {
+    //     let v:Vec<u32> = (1u32..101).map(|x|x.pow(2)).collect::<Vec<u32>>();
+    //     b.iter(||super::exe(v.clone(),50));
+    // }
 }
